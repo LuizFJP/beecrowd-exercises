6f1967015ef69305a52dac1b85c4636c37e0cec3
LuizFJP <luizfjportela@gmail.com>
2023-01-03 13:42:00 -0300

    squared and cubic

---
+++ Go/squared-and-cubic/main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+func main() {
+	var numberOfLines float64
+
+	fmt.Scanf("%f", &numberOfLines)
+
+	for i := 1.0; i <= numberOfLines; i++ {
+		fmt.Printf("%1.f %1.f %1.f\n", i, math.Pow(i, 2), math.Pow(i, 3))
+	}
+}
