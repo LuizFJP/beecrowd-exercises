92771ab6fdae45a70da08103f6a6d46a40122002
LuizFJP <luizfjportela@gmail.com>
2023-01-28 17:59:40 -0300

    line in array

---
+++ Go/line-in-array/main.go
@@ -0,0 +1,37 @@
+package main
+
+import "fmt"
+
+var (
+	numbers   [12][12]float64
+	line      int
+	operation string
+)
+
+func main() {
+	fmt.Scanf("%d", &line)
+	fmt.Scanf("%s", &operation)
+
+	for i := 0; i < len(numbers); i++ {
+		for j := 0; j < len(numbers[i]); j++ {
+			var number float64
+			fmt.Scanf("%f", &number)
+			numbers[i][j] = number
+		}
+	}
+
+	if operation == "S" {
+		fmt.Printf("%.1f\n", sumLine())
+	} else {
+		fmt.Printf("%.1f\n", sumLine() / float64(len(numbers)))
+	}
+
+}
+
+func sumLine() float64 {
+	var sum float64
+	for i := 0; i < len(numbers); i++ {
+		sum += numbers[line][i]
+	}
+	return sum
+}
